807048f97d4236371e59f2db7bdbbe2f20a4ab5a
fr4nc0 <francescogargiulo@inwind.it>
2015-05-20 11:21:16 +0200

    builder1

---
+++ CiraPatterns/src/it/cira/patterns/creational/builder1/TestBuilderPattern.java
@@ -0,0 +1,16 @@
+package it.cira.patterns.creational.builder1;
+
+
+ 
+public class TestBuilderPattern {
+ 
+    public static void main(String[] args) {
+          
+    	Computer comp = new Computer.ComputerBuilder(
+                "500 GB", "2 GB").setBluetoothEnabled(true)
+                .setGraphicsCardEnabled(true).build();
+        
+        System.out.println(comp.getHDD() + " " + comp.getRAM() + " " + comp.isBluetoothEnabled() + " " + comp.isGraphicsCardEnabled()); 
+    }
+ 
+}
